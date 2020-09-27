5f8c938b7fbbdc3b46c5e48a734288e9de5a7b36
ishansingla576 <63948415+ishansingla576@users.noreply.github.com>
2020-09-27 18:56:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var Database;
+var form, player,game;
+var GameState = 0;
+var playerCount = 0;
+function setup()
+{
+    Database = firebase.database();
+    createCanvas(500,500);   
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+function draw()
+{   
+    background(255);
+    drawSprites();
+    if(playerCount === 4)
+    {
+        GameState = 1;
+    }
+}
